11f939ae0187d97422f80f5522bc1f92f7a769f5
Brendan <brendanvdmeulen@gmail.com>
2025-05-26 17:25:38 +0200

    Register bar basics

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -8,7 +8,7 @@
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
                     <span class="flex h-12 w-12 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-10 fill-current text-black dark:text-white" />
+                        <x-app-logo-icon class="size-12 fill-current text-black dark:text-white" />
                     </span>
                     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
                 </a>
